ed049dd4d3612c7154e01b937b815b2384ec43ea
Alex Sherman <asherman.ca@gmail.com>
2023-05-09 16:14:22 -0700

    functional

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth'
+import type { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+// import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+	secret: process.env.NEXTAUTH_SECRET!,
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID!,
+			clientSecret: process.env.GITHUB_SECRET!,
+		}),
+	],
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+'use client'
 import Image from 'next/image'
+import { signIn } from 'next-auth/react'
 
 export default function Home() {
-	return <main></main>
+	return (
+		<main>
+			main
+			<div onClick={() => signIn('github')}>signin</div>
+		</main>
+	)
 }
